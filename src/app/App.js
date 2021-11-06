428a9099874f93eb1418991b4d8a54fcd3184c91
andreaeichenberger <andreaeichenberger.ae@gmail.com>
2021-11-06 01:16:50 -0300

    Starting to capture form values

--- src/app/App.js
+++ src/app/App.js
@@ -1,13 +1,24 @@
 import React, { Component } from "react";
 
 import WeatherInfo from "./components/WeatherInfo";
+import WeatherForm from "./components/WeatherForm";
 
 class App extends Component {
+
+    getWeather = e => {
+        const { city, country } = e.target.elements;
+        const cityValue = city.value;
+        const countryValue = country.value;
+
+        e.preventDefault();
+    }
+
     render() {
         return (
             <div className= "container p-4">
                 <div className= "row">
-                    <div className= "col-md-6">
+                    <div className= "col-md-6 mx-auto">
+                        <WeatherForm getWeather={this.getWeather}/>
                         <WeatherInfo/>
                     </div>
                 </div>
--- src/app/components/WeatherForm.js
+++ src/app/components/WeatherForm.js
@@ -1,7 +1,19 @@
 import React from "react";
 
 const weatherForm = props => (
-    <div className= "card card-body">
-        <form></form>
+    <div className="card card-body">
+        <form onSubmit={props.getWeather}>
+            <div className="form-group">
+                <input type="text" name="city" placeholder="Your city name" className="form-control" autoFocus/>
+            </div>
+            <div className="form-group">
+                <input type="text" name="country" placeholder="Your country name" className="form-control"/>
+            </div>
+            <button className="btn btn-success btn-block">
+                Get Weather
+            </button>
+        </form>
     </div>
 )
+
+export default weatherForm;
