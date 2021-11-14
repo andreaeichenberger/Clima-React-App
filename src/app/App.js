8241a1730d3fbec598c34f5f19c4673e24c748ce
andreaeichenberger <andreaeichenberger.ae@gmail.com>
2021-11-13 22:16:58 -0300

    Configuring data of  WeatherInfo

--- src/app/App.js
+++ src/app/App.js
@@ -7,6 +7,16 @@ import { WEATHER_KEY } from "./keys";
 
 class App extends Component {
 
+    state = {
+        temperature: "",
+        description: "",
+        humidity: "",
+        wind_speed: "",
+        city: "",
+        country: "",
+        error: null
+    }
+
     getWeather = async e => {
         e.preventDefault();
 
@@ -14,20 +24,30 @@ class App extends Component {
         const cityValue = city.value;
         const countryValue = country.value;
 
-        const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${cityValue},${countryValue}&appid=${WEATHER_KEY}`;
+        const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${cityValue},${countryValue}&appid=${WEATHER_KEY}&units=metric`;
         const response = await fetch(API_URL);
         const data = await response.json();
 
-        console.log(data)
+        console.log(this.state);
+
+        this.setState({
+            temperature: data.main.temp,
+            description: data.weather[0].description,
+            humidity: data.main.humidity,
+            wind_speed: data.wind.speed,
+            city: data.name,
+            country: data.sys.country,
+            error: null
+        }, () => console.log(this.state))
     }
 
     render() {
         return (
-            <div className= "container p-4">
-                <div className= "row">
-                    <div className= "col-md-6 mx-auto">
-                        <WeatherForm getWeather={this.getWeather}/>
-                        <WeatherInfo/>
+            <div className="container p-4">
+                <div className="row">
+                    <div className="col-md-6 mx-auto">
+                        <WeatherForm getWeather={this.getWeather} />
+                        <WeatherInfo {...this.state} />
                     </div>
                 </div>
             </div>
